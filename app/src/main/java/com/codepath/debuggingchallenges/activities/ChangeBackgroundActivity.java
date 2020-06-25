960b2010155441cf1ad870a3cdae0004ff0798c7
jacqueline-he <jacquelinehe00@gmail.com>
2020-06-25 09:57:13 -0700

    Complete debugging activities

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -3,7 +3,10 @@ package com.codepath.debuggingchallenges.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.codepath.debuggingchallenges.R;
 
@@ -15,12 +18,15 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+        Log.i("onCreate", "Over here!!");
     }
 
     public void onGoClick(View view) {
+        Log.i("onClick", "Over here!");
         View mainView = findViewById(android.R.id.content);
         mainView.setBackgroundColor(getNextColor());
     }
+
 
     private int getNextColor() {
         int newColor = (oldColor == Color.BLUE) ? Color.RED : Color.BLUE;
